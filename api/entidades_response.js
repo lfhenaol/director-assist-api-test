8de80b86b3fd6e8ebdb63e48b24d4eea53be1d7e
lfhenaol <lfhenaolopez@misena.edu.co>
2024-03-26 14:29:34 +0000

    Actualiza los endpoint dummy

---
+++ api/entidades_response.js
@@ -0,0 +1,147 @@
+export const LIST_ENTIDADES = [
+    {
+        "label": "Banco",
+        "seleccionado": false,
+        "value": "13"
+    },
+    {
+        "label": "COREN",
+        "seleccionado": false,
+        "value": "5"
+    },
+    {
+        "label": "Cad\u00danico",
+        "seleccionado": false,
+        "value": "19"
+    },
+    {
+        "label": "Cart\u00f3rio Registro Civil",
+        "seleccionado": false,
+        "value": "26"
+    },
+    {
+        "label": "Defensoria P\u00fablica",
+        "seleccionado": false,
+        "value": "25"
+    },
+    {
+        "label": "Detran - DRV",
+        "seleccionado": false,
+        "value": "27"
+    },
+    {
+        "label": "IIMG",
+        "seleccionado": false,
+        "value": "8"
+    },
+    {
+        "label": "INCRA",
+        "seleccionado": false,
+        "value": "21"
+    },
+    {
+        "label": "INSS",
+        "seleccionado": false,
+        "value": "29"
+    },
+    {
+        "label": "IPSEMG",
+        "seleccionado": false,
+        "value": "3"
+    },
+    {
+        "label": "Junta Militar",
+        "seleccionado": false,
+        "value": "1"
+    },
+    {
+        "label": "Minas F\u00e1cil",
+        "seleccionado": false,
+        "value": "16"
+    },
+    {
+        "label": "PRODEMGE",
+        "seleccionado": false,
+        "value": "12"
+    },
+    {
+        "label": "Pol\u00edcia Federal",
+        "seleccionado": false,
+        "value": "4"
+    },
+    {
+        "label": "Pol\u00edcia Militar",
+        "seleccionado": false,
+        "value": "24"
+    },
+    {
+        "label": "Prefeitura - PROCON",
+        "seleccionado": false,
+        "value": "2"
+    },
+    {
+        "label": "Prefeitura Municipal",
+        "seleccionado": false,
+        "value": "17"
+    },
+    {
+        "label": "Protocolo Municipal",
+        "seleccionado": false,
+        "value": "15"
+    },
+    {
+        "label": "RECEITA FEDERAL - PAV",
+        "seleccionado": false,
+        "value": "22"
+    },
+    {
+        "label": "Receita Federal",
+        "seleccionado": false,
+        "value": "9"
+    },
+    {
+        "label": "SALA MINEIRA DO EMPREENDEDOR",
+        "seleccionado": false,
+        "value": "14"
+    },
+    {
+        "label": "SEAP",
+        "seleccionado": false,
+        "value": "10"
+    },
+    {
+        "label": "SEDESO",
+        "seleccionado": false,
+        "value": "28"
+    },
+    {
+        "label": "SEPLAG",
+        "seleccionado": false,
+        "value": "11"
+    },
+    {
+        "label": "SIAT",
+        "seleccionado": false,
+        "value": "20"
+    },
+    {
+        "label": "SINE",
+        "seleccionado": false,
+        "value": "6"
+    },
+    {
+        "label": "Superintend\u00eancia Regional de Sa\u00fade",
+        "seleccionado": false,
+        "value": "23"
+    },
+    {
+        "label": "Tr\u00e2nsito - MG",
+        "seleccionado": false,
+        "value": "7"
+    },
+    {
+        "label": "Vigil\u00e2ncia Sanit\u00e1ria",
+        "seleccionado": false,
+        "value": "18"
+    }
+  ];
--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
-import Fastify from 'fastify'
-import { LOGIN_RESPONSE } from './login_response'
+import Fastify from 'fastify';
+import { LOGIN_RESPONSE } from './login_response';
+import { LIST_ENTIDADES } from './entidades_response';
+import { makeid } from './utils';
 
 const app = Fastify({
   logger: true,
@@ -9,15 +11,11 @@ app.post("/login", async (request, reply) => {
   reply.type("application/json").code(200);
   console.log(request.body);
 
-  await sleep(Math.random() * 4000);
-
   return LOGIN_RESPONSE;
 });
 
 app.get("/info", async (request, reply) => {
   reply.type("application/json").code(200);
-
-  await sleep(Math.random() * 4000);
 
   return {
     data: {
@@ -53,8 +51,6 @@ app.get("/info", async (request, reply) => {
 app.get("/api", async (request, reply) => {
   reply.type("application/json").code(200);
 
-  await sleep(Math.random() * 4000);
-
   return {
     data: {
       dato_ppal: (Math.random() * 10000).toFixed(0),
@@ -66,10 +62,20 @@ app.get("/api", async (request, reply) => {
   };
 });
 
-app.get("/lista-turnos-citas", async (request, reply) => {
+fastify.post("/management/entidades/consultar-entidades", async (request, reply) => {
   reply.type("application/json").code(200);
 
-  await sleep(Math.random() * 4000);
+  await sleep(Math.random() * 1000);
+
+  return {
+    data: LIST_ENTIDADES,
+    message: "Success get Lista turnos citas - Panorama de la operaci\u00f3n",
+    success: true,
+  };
+});
+
+app.get("/lista-turnos-citas", async (request, reply) => {
+  reply.type("application/json").code(200);
 
   return {
     data: {
@@ -100,72 +106,7 @@ function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function makeid(length) {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-}
-
 export default async function handler(req, reply) {
   await app.ready()
   app.server.emit('request', req, reply)
 }
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Vercel + Fastify Hello World</title>
-    <meta
-      name="description"
-      content="This is a starter template for Vercel + Fastify."
-    />
-  </head>
-  <body>
-    <h1>Vercel + Fastify Hello World</h1>
-    <p>
-      This is a starter template for Vercel + Fastify. Requests are
-      rewritten from <code>/*</code> to <code>/api/*</code>, which runs
-      as a Vercel Function.
-    </p>
-    <p>
-        For example, here is the boilerplate code for this route:
-    </p>
-    <pre>
-<code>import Fastify from 'fastify'
-
-const app = Fastify({
-  logger: true,
-})
-
-app.get('/', async (req, res) => {
-  return res.status(200).type('text/html').send(html)
-})
-
-export default async function handler(req: any, res: any) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}</code>
-    </pre>
-    <p>
-    <p>
-      <a href="https://vercel.com/templates/other/fastify-serverless-function">
-      Deploy your own
-      </a>
-      to get started.
-  </body>
-</html>
-`
